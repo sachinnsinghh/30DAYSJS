425e476e99b55dbbe6d3c89ddaa91e2e2eaccf3b
sachinnsinghh <sachinsingh000094gmail.com>
2024-07-13 16:44:08 +0530

    Initial commit

---
+++ day1/index.js
@@ -0,0 +1,34 @@
+var firstVariable = 23;
+console.log(firstVariable);
+
+let secondVariable = 34;
+console.log(secondVariable);
+
+const bool = true;
+console.log(bool);
+
+let digits = 245;
+let str ='sachin singh';
+let boool = false;
+var abc= {
+    name: 'sachin',
+    age:22
+}
+
+let arr = [1,2,3,4,5];
+
+console.log(typeof(digits));
+console.log(typeof(str));
+console.log(typeof(boool));
+console.log(typeof(abc));
+console.log(typeof(arr));
+console.log(Array.isArray(arr)); 
+
+
+let a=78;
+a=23;
+console.log(a);
+
+const b=34;
+b=566;
+console.log(b);
